42b2fc2acd79d0315a639a975b6a3851b9d88635
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-08-25 13:27:39 +0800

    feat(project): initial

---
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ECS_ATL_Practical_Test
+//
+//  Created by Wei Jen Wang on 2022/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
