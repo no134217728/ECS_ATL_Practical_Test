4db5427582e1af5acef45a8d700511a23efa8dba
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-08-26 10:01:49 +0800

    feat(config): add the config for convenience

---
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/NetworkService.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkService.swift
+//  ECS_ATL_Practical_Test
+//
+//  Created by Wei Jen Wang on 2022/8/26.
+//
+
+import UIKit
+//import Moya
+
+class NetworkService: NSObject {
+    func fetchData() {
+        if Bundle.main.infoDictionary?["Debug Mode"] as? String == "1" {
+            
+        } else {
+            
+        }
+    }
+}
