a6036085321918102f9e604b298f45eaad60a9e1
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-08-27 21:02:42 +0800

    feat(dataAccess): add moya items

--- ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/NetworkService.swift
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/NetworkService.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
-//import Moya
+import Moya
 
-class NetworkService: NSObject {
-    func fetchData() {
-        if Bundle.main.infoDictionary?["Debug Mode"] as? String == "1" {
-            
-        } else {
-            
+enum GithubServices {
+    case GetUserListDefault
+    case GetUserListPage(since: Int, perPage: Int = 20)
+}
+
+extension GithubServices: TargetType {
+    var baseURL: URL {
+        let urlStr = "https://api.github.com"
+        return URL(string: urlStr)!
+    }
+    
+    var path: String {
+        switch self {
+        case .GetUserListDefault, .GetUserListPage:
+            return "/users"
         }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        switch self {
+        default:
+            guard let url = Bundle.main.url(forResource: "MockData200", withExtension: "json"),
+                let data = try? Data(contentsOf: url) else {
+                    return Data()
+            }
+            return data
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .GetUserListPage(since, perPage):
+            return .requestParameters(parameters: ["since": since,
+                                                   "per_page": perPage], encoding: URLEncoding.queryString)
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [:]
     }
 }
