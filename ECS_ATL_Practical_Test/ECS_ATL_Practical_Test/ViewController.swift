9a9fc4cdf5eb13317d6e1feeb4db0b22d58800a4
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-09-01 14:51:44 +0800

    feat(mainScreen): add required basic code

--- ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/ViewController.swift
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class ViewController: UIViewController {
     @IBOutlet weak var userListTableView: UITableView!
+        
+    let disposeBag = DisposeBag()
+    let viewModel = ViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userListTableView.delegate = self
         
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
