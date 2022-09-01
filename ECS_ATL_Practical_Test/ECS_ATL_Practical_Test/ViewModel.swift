e8cb042a8e6663b3450fb433ae59ed134f70cf59
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-09-01 15:05:12 +0800

    feat(viewModel): add the function for normal fetch
    data directly be used on viewController

--- ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/ViewModel.swift
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/ViewModel.swift
@@ -16,4 +16,24 @@ class ViewModel {
     let useMock = Bundle.main.infoDictionary?["Mock Mode"] as? String == "1"
     
     private let disposeBag = DisposeBag()
+    func fetchUserListAll() -> Observable<[UserListDetails]> {
+        return Observable<[UserListDetails]>.create { [self] observer -> Disposable in
+            let stub = useMock ? MoyaProvider<GithubServices>.immediatelyStub : MoyaProvider<GithubServices>.neverStub
+            let provider = MoyaProvider<GithubServices>(stubClosure: stub)
+            return provider.rx.request(.GetUserListDefault).subscribe { event in
+                    switch event {
+                    case .success(let res):
+                        guard let userListData = try? JSONDecoder().decode(UserList.self, from: res.data) else {
+                            print("no data")
+                            return
+                        }
+                        
+                        observer.onNext(userListData.users)
+                        observer.onCompleted()
+                    case .failure(let err):
+                        print(err)
+                    }
+            }
+        }
+    }
 }
