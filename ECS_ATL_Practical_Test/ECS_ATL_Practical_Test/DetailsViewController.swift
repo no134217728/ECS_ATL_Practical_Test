c199839e42c37f77de6910779c86d309fb511940
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-09-01 16:27:45 +0800

    feat(detailsPage): add the class file and basic items

---
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/DetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailsViewController.swift
+//  ECS_ATL_Practical_Test
+//
+//  Created by Wei Jen Wang on 2022/9/1.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var userAvatar: UIImageView!
+    @IBOutlet weak var userID: UILabel!
+    @IBOutlet weak var isStaff: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
