10433b700f4e08e30183e37ea3f04a60c06f9875
Wei Jen Wang <weijen.wang@tpisoftware.com>
2022-09-01 16:29:35 +0800

    feat(detailsPage): add packages and remove dirties

--- ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/DetailsViewController.swift
+++ ECS_ATL_Practical_Test/ECS_ATL_Practical_Test/DetailsViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DetailsViewController: UIViewController {
     @IBOutlet weak var closeButton: UIButton!
@@ -16,19 +18,7 @@ class DetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
